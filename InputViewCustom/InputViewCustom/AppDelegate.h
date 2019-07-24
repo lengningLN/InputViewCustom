e3ef272dd1a03c93a5d54d5f4cdb9f1444e601f0
liuning <18500088395@163.com>
2019-07-24 11:00:44 +0800

    初始化工程

---
+++ InputViewCustom/InputViewCustom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InputViewCustom
+//
+//  Created by 刘宁 on 2019/7/24.
+//  Copyright © 2019 刘宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InputViewCustom/InputViewCustom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InputViewCustom
+//
+//  Created by 刘宁 on 2019/7/24.
+//  Copyright © 2019 刘宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
